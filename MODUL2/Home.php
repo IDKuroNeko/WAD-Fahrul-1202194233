14d287a28f8f8c5a509656cae17e76611f3450f4
IDKuroNeko <ahmadfachrulroji@student.telkomuniversity.ac.id>
2021-10-25 06:25:14 +0700

    Commit TP Modul 2

---
+++ MODUL2/Home.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <style type="text/css">
+      body{
+        background-color: #EDF6E5;
+      }
+      a:hover{
+        background-color: #D5D5D5;
+      }
+      </style>
+
+    <title>ESD Venue</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: #B5EAEA;">
+  <div class="navbar mx-auto">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Booking.php">Booking</a>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <h3 style="text-align : center; weight : bold">WELCOME TO ESD VENUE RESERVATION</h3>
+
+    <div class="content" style="background-color: #B5EAEA;">
+      <div class="cari" style="text-align: center; color: black;">
+        <p>Find Your Best Deal For Your Event, Here!</p>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://weddingmarket.com/storage/images/artikelidea/d43df113c6c34c7ed6d41c8f36521052cf262ff9.webp" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Nusantara Hall</h5>
+            <p class="card-text">$2000 / Hour <br> 5000 Capacity</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item" style="font-weight: bold; color: green;">Free Parking</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Full AC</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Cleaning Service</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Covid-19 Health Protocol</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="Booking.php" class="btn btn-primary">Book Now</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://obs.line-scdn.net/0hZCrH5gO3BWxPMS4YEJh6O3VnBgN8XRZvKwdUch9fW1g3AUA-cAAfWWM1D1hiVUIyIQBNAmIzHl0yBBU8ewMe/w644" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Garuda Hall</h5>
+            <p class="card-text">$1000 / Hour <br> 2000 Capacity</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item" style="font-weight: bold; color: green;">Free Parking</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Full AC</li>
+            <li class="list-group-item" style="font-weight: bold; color: red;">No Cleaning Service</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Covid-19 Health Protocol</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="Booking.php" class="btn btn-primary">Book Now</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card" style="width: 18rem;">
+          <img src="https://i1.wp.com/blog.venuerific.com/wp-content/uploads/2016/04/grand-hyatt-jakarta-penthouse-corporate-function-event-venue-indonesia1.jpg?resize=1160%2C771&ssl=1" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Gedung Serba Guna</h5>
+            <p class="card-text">$500 / Hour <br> 500 Capacity</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item" style="font-weight: bold; color: red;">No Free Parking</li>
+            <li class="list-group-item" style="font-weight: bold; color: red;">No Full AC</li>
+            <li class="list-group-item" style="font-weight: bold; color: red;">No Cleaning Service</li>
+            <li class="list-group-item" style="font-weight: bold; color: green;">Covid-19 Health Protocol</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="Booking.php" class="btn btn-primary">Book Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+        <footer style="text-align: center; margin-top: 250px;">Create By : Fachrul_1202194233</footer>
+  </body>
+</html>
