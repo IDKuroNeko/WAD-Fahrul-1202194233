528a6d3d36490891dbfbe57057aad3222b54b115
IDKuroNeko <ahmadfachrulroji@student.telkomuniversity.ac.id>
2021-11-21 22:41:57 +0700

    Push TP Modul 3

---
+++ MODUL3 FACHRUL/Fahrul_modul3.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2021 at 04:40 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku_table`
+--
+
+CREATE TABLE `buku_table` (
+  `id_buku` int(100) NOT NULL,
+  `judul_buku` varchar(255) NOT NULL,
+  `penulis_buku` varchar(255) NOT NULL,
+  `tahun_terbit` int(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tag` varchar(255) NOT NULL,
+  `bahasa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku_table`
+--
+
+INSERT INTO `buku_table` (`id_buku`, `judul_buku`, `penulis_buku`, `tahun_terbit`, `deskripsi`, `gambar`, `tag`, `bahasa`) VALUES
+(6, 'Pemrograman', 'Fachrulroji_1202194233', 2019, 'Berisi tentang pemrograman dasar', '613630_3cf8c791-d807-4c68-8dd6-bf98c662764d_654_1008.jpg', 'Pemrograman', 'Indonesia'),
+(7, 'Kalkulus', 'Fachrulroji_1202194233', 2018, 'Buku berisi tentang materi aljabar', 'WhatsApp_Image_2019_08_16_at_084749_2_.jpeg', 'Kalkulus', 'Indonesia');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku_table`
+--
+ALTER TABLE `buku_table`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku_table`
+--
+ALTER TABLE `buku_table`
+  MODIFY `id_buku` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
